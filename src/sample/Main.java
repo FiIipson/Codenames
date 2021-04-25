32494612cce4de98b480c8eaa01168189bab65b1
FiIipson <filip.jasinski@student.uj.edu.pl>
2021-04-25 23:36:04 +0200

    Something

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,12 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
