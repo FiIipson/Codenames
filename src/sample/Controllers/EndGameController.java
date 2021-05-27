9cb3a392c34744bc6e436fbacb2c556c4440a9a6
FiIipson <filip.jasinski@student.uj.edu.pl>
2021-05-27 16:11:30 +0200

    End screen text

--- src/sample/Controllers/EndGameController.java
+++ src/sample/Controllers/EndGameController.java
@@ -6,12 +6,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
 public class EndGameController {
+  String blue_win = "Blue team wins by revealing all the cards";
+  String red_win = "Red team wins by revealing all the cards";
+  String red_bomb = "Red team wins by detonating enemies";
+  String blue_bomb = "Blue team wins by detonating enemies";
+  @FXML
+  Text to_be_shown;
+  @FXML
+  public void initialize() {
+    if (GlobalVar.result_ == GlobalVar.Result.BLUE) {
+      to_be_shown.setText(blue_win);
+    }
+    else if (GlobalVar.result_ == GlobalVar.Result.RED) {
+      to_be_shown.setText(red_win);
+    }
+    else if (GlobalVar.result_ == GlobalVar.Result.RED_BOMB) {
+      to_be_shown.setText(blue_bomb);
+    }
+    else {
+      to_be_shown.setText(red_bomb);
+    }
+  }
   @FXML
   public void toGame(ActionEvent event) throws IOException {
     GlobalVar.setWords(GlobalVar.difficulty);
